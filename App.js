bff8de5a1ab2ead6bffded895bed04e13b71f7e7
Abel McElroy <abel.h.mcelroy@gmail.com>
2023-07-01 15:27:50 -0400

    dynamic backend url

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const apiUrl = Constants.expoConfig.extra.apiUrl;
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! "{apiUrl}"</Text>
       <StatusBar style="auto" />
     </View>
   );
